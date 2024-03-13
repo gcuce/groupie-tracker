8fedaf812460b9a0a6b664bbb57254c7f363c90d
sudemir <sudenaz_demir_1903@outlook.com>
2024-03-13 14:09:46 +0300

    artists part is updated

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 type Artist struct {
 	Name       string   `json:"name"`
 	Image      string   `json:"image"`
-	Year       int      `json:"year"`
+	Year       int      `json:"creationDate"`
 	FirstAlbum string   `json:"firstAlbum"`
 	Members    []string `json:"members"`
 }
